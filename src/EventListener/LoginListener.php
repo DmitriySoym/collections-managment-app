f4f1562871e05f3090d23aa17c17dc19a2fc55bb
Dmitriy Soymchenko <d.soymchenko@gmail.com>
2024-05-15 10:36:11 +0300

    feat: add login event listener

---
+++ src/EventListener/LoginListener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\EventListener;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+final class LoginListener
+{
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[AsEventListener(event: 'security.interactive_login')]
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event): void
+    {
+        $user = $event->getAuthenticationToken()->getUser();
+        $user->setLastLogin(new \DateTimeImmutable());
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+    }
+}
