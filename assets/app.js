22ee649ed57231442eef16928876345e9eade445
Dmitriy Soymchenko <d.soymchenko@gmail.com>
2024-05-17 12:02:47 +0300

    refactor: change js bootstrap apply

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/app.css";
-require("bootstrap");
+
 // import "bootstrap/dist/js/bootstrap.min.js";
 
 // import { createApp } from "vue";
