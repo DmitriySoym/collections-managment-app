ff372425b85459bab37d8db5467ec79d5e3e4950
Dmitriy Soymchenko <d.soymchenko@gmail.com>
2024-05-20 11:21:47 +0300

    feat: add category name validation

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,9 +4,10 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CategoryType extends AbstractType
 {
@@ -15,6 +16,17 @@ class CategoryType extends AbstractType
         $builder
             ->add('name', null, [
                 'label' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter collection name',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'max' => 30,
+                        'minMessage' => 'Your collection name should be at least {{ limit }} characters',
+                        'maxMessage' => 'Your collection name should be at most {{ limit }} characters',
+                    ]),
+                ],
             ])
             // ->add('create', SubmitType::class)
         ;
