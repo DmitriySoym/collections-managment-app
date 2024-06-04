8d510fc1dc70efe21594dad01e1044754d757a5c
Dmitriy Soymchenko <d.soymchenko@gmail.com>
2024-06-04 12:33:59 +0300

    refactor: remove webpack from project

--- public/js/app.js
+++ public/js/app.js
@@ -5,11 +5,6 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/awesomefonts/css/all.min.css";
-import "./styles/app.css";
-
 window.onload = function () {
     document.body.classList.add("loaded_hiding");
     window.setTimeout(function () {
