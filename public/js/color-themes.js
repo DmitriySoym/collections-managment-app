2d95b98c3ba98f3036736fe6cb46d1b34cfa26e9
Dmitriy Soymchenko <d.soymchenko@gmail.com>
2024-06-09 10:35:12 +0300

    refactor: change footer sryles

--- public/js/color-themes.js
+++ public/js/color-themes.js
@@ -2,9 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const lightThemBtn = document.querySelector("#light-theme-button");
     const darkThemBtn = document.querySelector("#dark-theme-button");
 
-    // document.body.setAttribute("data-theme", localStorage.getItem("theme"));
-    // document.body.classList.add(localStorage.getItem("theme"));
-
     function setColorTheme(theme) {
         console.log(theme);
         document.body.setAttribute("data-bs-theme", theme);
